10cce972a604ae703ce4d276acae9b6868dd5965
Krzysztof <krzyspanfilewicz@gmail.com>
2024-11-15 12:49:25 +0100

    Zad2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ public class Main {
         String filepath = "liczby.txt";
         ArrayList<String> liczby = Download(filepath);
         System.out.println(zad1(liczby));
+        ArrayList<String>wynik=zad2(liczby);
+        for (String i:wynik){
+            System.out.println(i);
+        }
     }
 
     public static ArrayList<String> Download(String filepath) {
@@ -44,4 +48,34 @@ public class Main {
         }
         return count;
     }
+    public static ArrayList<String>zad2(ArrayList<String>liczby){
+        ArrayList<String>Wynik=new ArrayList<>();
+        int naj=0;
+        for(String i:liczby){
+            if(!i.equals("")) {
+                int num = Integer.parseInt(i,2);
+                if (num > naj) {
+                    naj = num;
+                }
+            }
+        }
+        Wynik.add(Integer.toString(naj));
+        Wynik.add(decimalToBinary(naj));
+        return Wynik;
     }
+   public static String decimalToBinary(int n)
+    {
+
+
+
+        int remainder, quotient = n;
+        StringBuilder binaryNum = new StringBuilder();
+        while (quotient > 0) {
+            remainder = quotient % 2;
+            binaryNum.insert(0, Integer.toString(remainder));
+            quotient = quotient / 2;
+        }
+        return binaryNum.toString();
+    }
+
+}
